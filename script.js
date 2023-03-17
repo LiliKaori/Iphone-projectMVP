8ee9bc13e77619a4cb13e4b25f154e3725c0fbe1
LiliKaori <lilikaori10@gmail.com>
2023-03-17 20:30:44 -0300

    Projeto finalizado

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function changeColor(cor) {
+    let circle = document.querySelector(".circle")
+    circle.style.background = cor    
+}
+
+function changeImage(image) {
+    let iPhone = document.querySelector(".iphone")
+    iPhone.src= image
+}
